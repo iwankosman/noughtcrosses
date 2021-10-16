5f73459c6de0a9f6459c7d1488cc2a0f30dcc5bb
Iwan <kosya1992@mail.ru>
2021-10-16 13:58:58 +0300

    Initial commit

---
+++ src/main/java/com/test/noughtcrosses/NoughtCrossesApplication.java
@@ -0,0 +1,13 @@
+package com.test.noughtcrosses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoughtCrossesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoughtCrossesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/noughtcrosses/NoughtCrossesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.noughtcrosses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoughtCrossesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
